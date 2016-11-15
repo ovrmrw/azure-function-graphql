0cfcb9c23991e760e4229c66eff7a4093648f8da
ovrmrw <t.ngc.0403@gmail.com>
2016-11-15 14:50:45 +0900

    Update

--- lodash-test/index.ts
+++ lodash-test/index.ts
@@ -1,23 +1,18 @@
 import * as lodash from 'lodash';
 
-export function azureFunction(context, req) {
-  context.log('Node.js HTTP trigger function processed a request. RequestUri=%s', req.originalUrl);
+const authConfig = require('../auth0.secret.json');
 
-  // if (req.query.name || (req.body && req.body.name)) {
-  //     context.res = {
-  //         // status: 200, /* Defaults to 200 */
-  //         body: "Hello GitHub, " + (req.query.name || req.body.name)
-  //     };
-  // }
-  // else {
-  //     context.res = {
-  //         status: 400,
-  //         body: "Please pass a name on the query string or in the request body"
-  //     };
-  // }
+const auth0: (fn: Function) => Function = require('azure-functions-auth0')({
+  clientId: authConfig.clientId,
+  clientSecret: authConfig.clientSecret,
+  domain: authConfig.domain,
+});
+
+
+export const azureFunction = auth0(function (context, req) {
   context.res = {
     status: 200,
     body: 'now: ' + lodash.now()
   };
   context.done();
-};
+});
