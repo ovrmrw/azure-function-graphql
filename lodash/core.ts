7e00720f857e20fe0fd390df1780921da1cd10b7
ovrmrw <t.ngc.0403@gmail.com>
2016-11-16 23:24:54 +0900

    Fix

--- lodash/core.ts
+++ lodash/core.ts
@@ -4,16 +4,14 @@ import { AzureFunction } from '../types';
 
 
 export const lodashAzureFunction: AzureFunction =
-  (context, req) => {
-    return (async () => {
-      context.res = {
-        status: 200,
-        body: 'now: ' + now()
-      };
+  async (context, req) => {
+    context.res = {
+      status: 200,
+      body: 'now: ' + now()
+    };
 
-      await new Promise(resolve => setTimeout(resolve, 2000));
-      context.done();
-    })();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    context.done();
   };
 
 
