b12b4621787c36650c08502f27e1a9822feaf23e
ovrmrw <t.ngc.0403@gmail.com>
2016-11-17 15:02:18 +0900

    Fix

--- createCustomToken/main.ts
+++ createCustomToken/main.ts
@@ -5,6 +5,13 @@ import { AzureFunction } from '../types';
 export const firebaseAzureFunction: AzureFunction =
   async (context, req) => {
     const uid = req.query.user_id || req.body.user_id;
+    if (!uid) {
+      context.res = {
+        status: 400,
+        body: { error: 'user_id is needed.' }
+      };
+      return context;
+    }
 
     try {
       const customToken: string = await firebaseApp.auth().createCustomToken(uid);
@@ -13,10 +20,10 @@ export const firebaseAzureFunction: AzureFunction =
         body: { customToken }
       };
       // context.done();
-    } catch (err) {
+    } catch (error) {
       context.res = {
         status: 400,
-        body: err
+        body: { error }
       };
       // context.done();
     }
