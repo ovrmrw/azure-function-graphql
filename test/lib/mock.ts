0a6bd8a0e9bf576dc7b4285f398c54a0f1309946
ovrmrw <t.ngc.0403@gmail.com>
2016-11-17 23:25:34 +0900

    Fix

--- test/lib/mock.ts
+++ test/lib/mock.ts
@@ -4,6 +4,7 @@ import { AFContext, AFRequest } from '../../types';
 export const mockContext: AFContext = {
   res: {},
   done: () => { },
+  log: (message?: any, ...params: any[]) => console.log(message, params),
 };
 
 
