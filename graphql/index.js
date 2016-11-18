b00e547cd922309b6c47fd7603cb0ef9c4f49063
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2016-11-18 09:14:30 +0900

    Cleanup

--- graphql/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var main_1 = require("./main");
-Object.defineProperty(exports, "__esModule", { value: true });
-// export default auth0AuthenticationHook(graphqlAzureFunction);
-exports.default = main_1.graphqlAzureFunction;
