35dbccd3e8d5fadd4fbdfcbf7c677116cd84a98c
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2016-11-18 11:35:37 +0900

    Fix

--- lodash/index.ts
+++ lodash/index.ts
@@ -2,4 +2,5 @@ import { lodashAzureFunction } from './main';
 import { auth0AuthenticationHook } from '../lib/auth';
 
 
-export default auth0AuthenticationHook(lodashAzureFunction);
+// export default auth0AuthenticationHook(lodashAzureFunction);
+export default lodashAzureFunction;
